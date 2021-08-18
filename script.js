a47dcbbf7fef534921e316802310ac919b9a3719
Zackaria S <79061155+Zackaria-Slimane@users.noreply.github.com>
2021-08-18 23:59:33 +0100

    clean up

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import {initGame, populateColorsGrid} from "./colorGame.js"
 
 const nextColorButton = document.querySelector(".nextColor-btn")
-
 // initGame()
 
 nextColorButton.addEventListener("click", () => {
